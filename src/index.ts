b2e6a6d45357a20568a87bcfceb46518d3c0ef52
kvbxss <kubadownar444@gmail.com>
2024-05-06 17:59:03 +0200

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from './routes';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const MONGO_URL = 'mongodb+srv://jakubroot:jakubroot@crud.3qq4mg1.mongodb.net/?retryWrites=true&w=majority&appName=crud';
 mongoose.connect(MONGO_URL, {
